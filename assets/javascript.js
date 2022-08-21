ccb284b6d76601a91e721f5bc09a6f29acf25b10
Gloriana Gonzalez <glogonzalez@Glorianas-MBP.lan>
2022-08-20 19:30:12 -0500

    created CSS selectors

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,8 +1,14 @@
 
 var timeLeft = document.querySelector("#timer");
 var mainEl = document.getElementById("main");
+var yes = document.getElementById("#yes");
+var no = document.getElementById("#no");
+var questions = document.getElementById(".questions")
+var questions = document.getElementById(".answers")
 
 var secondsLeft = 60;
+
+document.getElementById(".questions").style.display = "none";
 
 function setTime() {
   var timerInterval = setInterval(function() {
@@ -17,8 +23,6 @@ function setTime() {
   }, 1000);
 }
 
-setTime(); 
-
 function sendMessage() {
   timeEl.textContent = " ";
   var imgEl = document.createElement("img");
@@ -26,3 +30,6 @@ function sendMessage() {
   mainEl.appendChild(imgEl);
 
 }
+
+setTime(); 
+sendMessage(); 
