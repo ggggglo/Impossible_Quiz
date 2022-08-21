915807fdc4b7f3582df11c4622adc539128ff2f8
Gloriana Gonzalez <glogonzalez@Glorianas-MBP.lan>
2022-08-21 14:12:01 -0500

    created click events for the yes button and the first question

--- assets/javascript.js
+++ assets/javascript.js
@@ -3,24 +3,39 @@ var timeLeft = document.querySelector("#timer");
 var mainEl = document.querySelector("main");
 var yes = document.querySelector("#yes");
 var no = document.querySelector("#no");
+var house = document.querySelector("#house");
 var question1 = document.querySelector("#question1")
 var answer1 = document.querySelector("#answer1")
+var text1 = document.querySelector("#text1")
 var question2 = document.querySelector("#question2")
 var answer2 = document.querySelector("#answer2")
 var question3 = document.querySelector("#question3")
 var answer3 = document.querySelector("#answer3")
-var header = document.querySelector("h1")
+var h1 = document.querySelector("h1")
+var h2 = document.querySelector("h2")
 
 var secondsLeft = 60;
+var wins = 0;
+var losses = 0;
+
+
 
 yes.addEventListener("click", function(){
     question1.style.display = "block";
-    answ
+    answer1.style.display = "block";
     timeLeft.style.display = "block";
     yes.style.display = "none";
     no.style.display = "none";
-    header.style.display = "none"
+    h1.style.display = "none"
+    h2.style.display = "none";
     });
+
+house.addEventListener("click",function(){
+    wins ++
+    question1.style.display = "none";
+    answer1.style.display = "none";
+    text1.style.display = "block";
+})  
 
 function setTime() {
   var timerInterval = setInterval(function() {
@@ -32,14 +47,6 @@ function setTime() {
       sendMessage();
     }
   }, 1000);
-}
-
-function sendMessage() {
-  timeEl.textContent = " ";
-  var imgEl = document.createElement("img");
-  imgEl.setAttribute("src", "images/image_1.jpg");
-  mainEl.appendChild(imgEl);
-
 }
 
 setTime(); 
